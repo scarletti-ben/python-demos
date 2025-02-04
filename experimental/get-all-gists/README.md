282911ddc0db9854623891bd66c325d8c3a07e8d
scarletti.ben@gmail.com <scarletti.ben@gmail.com>
2025-02-04 20:21:38 +0000

    tweaks

--- experimental/get-all-gists/README.md
+++ experimental/get-all-gists/README.md
@@ -3,3 +3,26 @@
 You can use the `GitHub` `API` to access all public `gists` for a given user
 
 This is easily achieved in `Python` via `requests.get(https://api.github.com/users/{username}/gists)`, which will return a list of dictionaries of all the `gists` for that username
+
+An example, abridged, log output for a user with only one `gist`
+```powershell
+[{'url': 'https://api.github.com/gists/{id}', 'forks_url': 'https://api.github.com/gists/{id}/forks', 'commits_url': 'https://api.github.com/gists/{id}/commits', 'id': '{id}', 'node_id': 'G_kwDOCMAZpxoAIGZjNDRlY2NjNxI1YTIzYjllYjNhYmYwOGM4YzBiZWQx', 'git_pull_url': 'https://gist.github.com/{id}.git', 'git_push_url': 'https://gist.github.com/{id}.git', 'html_url': 'https://gist.github.com/username/{id}', 'files': {'test.py': {'filename': 'test.py', 'type': 'application/x-python', 'language': 'Python', 'raw_url': 'https://gist.githubusercontent.com/username/{id}/raw/48137ab03450d53x4301xe3fc35853fd1e0fcd2f/test.py', 'size': 116}}, 'public': True, 'created_at': '2025-02-04T17:46:20Z', 'updated_at': '2025-02-04T17:46:21Z', 'description': 'test gist', 'comments': 0, 'user': None, 'comments_enabled': True, 'comments_url': 'https://api.github.com/gists/{id}/comments', 'owner': {'login': 'username', 'id': 146817206, 'node_id': 'U_kgDcXMAZpg', 'avatar_url': 'https://avatars.githubusercontent.com/u/146217206?v=4', 'gravatar_id': '', 'url': 'https://api.github.com/users/username', 'html_url': 'https://github.com/username', 'followers_url': 'https://api.github.com/users/username/followers', 'following_url': 'https://api.github.com/users/username/following{/other_user}', 'gists_url': 'https://api.github.com/users/username/gists{/gist_id}', 'starred_url': 'https://api.github.com/users/username/starred{/owner}{/repo}', 'subscriptions_url': 'https://api.github.com/users/username/subscriptions', 'organizations_url': 'https://api.github.com/users/username/orgs', 'repos_url': 'https://api.github.com/users/username/repos', 'events_url': 'https://api.github.com/users/username/events{/privacy}', 'received_events_url': 'https://api.github.com/users/username/received_events', 'type': 'User', 'user_view_type': 'public', 'site_admin': False}, 'truncated': False}]
+```
+
+Cleaned up output with new method
+```powershell
+
+-------------------------------------------------------------------------------
+
+fc44eccc725a23b9eb3abf08c8c0bed1: 
+    description: test gist
+    files: 
+        test.py: https://gist.githubusercontent.com/scarletti-ben/fc44eccc725a23b9eb3abf08c8c0bed1/raw/48137ab03450d53f4301ce3fc35853fd1e0fcd2f/test.py
+
+
+-------------------------------------------------------------------------------
+
+ID: fc44eccc725a23b9eb3abf08c8c0bed1 -> Description: test gist ->
+    File: test.py -> URL: https://gist.githubusercontent.com/scarletti-ben/fc44eccc725a23b9eb3abf08c8c0bed1/raw/48137ab03450d53f4301ce3fc35853fd1e0fcd2f/test.py
+
+```
--- experimental/get-all-gists/main.py
+++ experimental/get-all-gists/main.py
@@ -16,18 +16,94 @@ username: str = "scarletti-ben"
 # < Functionality
 # < ========================================================
 
-def get_all_gists(username: str) -> list[dict] | None:
+# def get_all_gist_metadata(username: str) -> list[dict] | None:
+#     """Get all public gists from GitHub for a given user"""
+#     url: str = f"https://api.github.com/users/{username}/gists"
+#     response: Response = requests.get(url)
+#     if response.status_code == 200:
+#         all_metadata: list[dict] = response.json()
+#         return all_metadata
+    
+# def get_gist_urls(metadata_list: list[dict]) -> list[dict] | None:
+#     """Get all gist URLs from the main metadata list"""
+#     gist_urls: list[str] = [metadata["html_url"] for metadata in metadata_list]
+#     return gist_urls
+
+# def get_files_in_gist(url: str) -> dict[str, str] | None:
+#     """Get a dictionary for all files in a given gist"""
+#     response: Response = requests.get(url)
+#     if response.status_code == 200:
+#         metadata: dict = response.json()
+#         output: dict = {}
+#         for file_name, file_info in metadata["files"].items():
+#             raw_url: str = file_info["raw_url"]
+#             output[file_name] = raw_url
+#         return output
+    
+# def get_text_from_raw_url(raw_url: str) -> str | None:
+#     """Get the raw text string from a raw file URL"""
+#     response: dict = requests.get(raw_url)
+#     if response.status_code == 200:
+#         return response.text
+
+def get_clean_dictionary(username: str) -> dict | None:
     """Get all public gists from GitHub for a given user"""
     url: str = f"https://api.github.com/users/{username}/gists"
     response: Response = requests.get(url)
     if response.status_code == 200:
-        gists: list[dict] = response.json()
-        return gists
-    
-# < ========================================================
-# < Execution
-# < ========================================================
+        all_metadata: list[dict] = response.json()
+        clean_gist_dictionary: dict = {}
+        for gist_metadata in all_metadata:
+            gist_id: int = gist_metadata["id"]
+            gist_description: str = gist_metadata["description"]
+            gist_files_dictionary: dict = gist_metadata["files"]
+            clean_files_dictionary: dict = {}
+            for file_name, file_dictionary in gist_files_dictionary.items():
+                file_url: str = file_dictionary["raw_url"]
+                clean_files_dictionary[file_name] = file_url
+            clean_gist_dictionary[gist_id] = {
+                "description": gist_description,
+                "files": clean_files_dictionary
+            }
+        return clean_gist_dictionary
+
+def pretty_print(item, indent = 0):
+    """Recursively pretty-print dictionaries and iterables"""
+
+    separator = '\n' + '-' * 79 + '\n'
+    tab = '    '
+
+    if indent == 0:
+        print(separator)
+
+    item = getattr(item, '__dict__', item)
+
+    if isinstance(item, dict):
+        for key, value in item.items():
+            print(f"{tab * indent}{key}: ", end = "")
+            if isinstance(value, (dict, list, tuple, set)):
+                print()
+                pretty_print(value, indent + 1)
+                if indent == 0:
+                    print()
+            else:
+                print(value)
+    elif isinstance(item, (list, tuple, set)):
+        print(f"{tab * indent}" + ", ".join(map(str, item)))
+    else:
+        print(f"{tab * indent}{item}")
+
+    if indent == 0:
+        print(separator)
 
 if __name__ == "__main__":
-    gists: list[dict] = get_all_gists(username)
-    print(gists)
+
+    core_dictionary: dict = get_clean_dictionary(username)
+    pretty_print(core_dictionary)
+
+    for gist_identifier, gist_dictionary in core_dictionary.items():
+        print(f"ID: {gist_identifier} -> Description: {gist_dictionary["description"]} ->")
+        for file_name, file_url in gist_dictionary["files"].items():
+            print(f"    File: {file_name} -> URL: {file_url}")
+    
+    print("\n\n")
